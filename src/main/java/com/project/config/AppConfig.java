483993eeaf1296a267c82c7216c5258f720357d9
rookieanvesh <srivastavaanvesh13@gmail.com>
2024-09-19 01:13:01 +0530

    jwt dep

--- src/main/java/com/project/config/AppConfig.java
+++ src/main/java/com/project/config/AppConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -47,6 +49,11 @@ public class AppConfig {
                 cfg.setMaxAge(3600L);
                 return cfg;
             }
+        };
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+            return new BCryptPasswordEncoder();
         }
     }
 }
